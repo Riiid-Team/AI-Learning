0070d3d7b0ce469021667db31d386d598276d3ea
Daniella Bojado <daniella.bojado@gmail.com>
2020-12-08 09:54:55 -0600

    add acquire

---
+++ acquire.py
@@ -0,0 +1,10 @@
+# Acquire File
+# Imports 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import os
+
+###################### Acquire Riiid Data ########################
