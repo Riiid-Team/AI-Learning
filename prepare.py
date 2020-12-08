6a6df82790bf30cb5162583dba90304fde9ab9c1
Daniella Bojado <daniella.bojado@gmail.com>
2020-12-08 09:55:22 -0600

    add prepare

---
+++ prepare.py
@@ -0,0 +1,13 @@
+# Prepare File 
+# Imports
+
+import pandas as pd
+import numpy as np
+import scipy as sp 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+from sklearn.model_selection import train_test_split
+
+###################### Prepare Riiid Data ########################
