4c50790ba96fe6515a30b3bbfffb33ce1e79143a
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-12-09 08:06:24 -0600

    Updated acquire file with docstrings, comments, and pyspark to preserve local memory/faster loading times

--- acquire.py
+++ acquire.py
@@ -7,11 +7,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-###################### Acquire Riiid Data ########################
-def get_riiid_data():
-    # Acquire data
-    df_train = pd.read_csv('train.csv')
-    df_lectures = pd.read_csv('lectures.csv')
-    df_questions = pd.read_csv('questions.csv')
+import pyspark
+from pyspark.sql import SparkSession
 
+
+###################### Acquire Riiid Data ########################
+
+
+def get_riiid_data():
+    '''
+    This function acquires `train`, `lectures`, and `questions` datasets and
+    returns a merged spark dataframe.
+
+    Parameters
+    ----------
+    None 
+    
+    Returns
+    -------
+    df :  pyspark.sql.dataframe.DataFrame
+    '''
+
+    spark = SparkSession.builder.getOrCreate()
+    
+    # Acquire data
+    df_train = spark.read.csv('train.csv', header=True, inferSchema=True)
+    df_lectures = spark.read.csv('lectures.csv', header=True, inferSchema=True)
+    df_questions = spark.read.csv('questions.csv', header=True, inferSchema=True)
+    
+    # Left join df_train and df_lectures using `content_id` as the primary key.
+    df_merged = df_train.join(df_lectures, df_train.content_id == df_lectures.lecture_id, how='left')
+    
+    # Left join df_merged and df_questions using `content_id` as the primary key.
+    df = df_merged.join(df_questions, df_merged.content_id == df_questions.question_id, how='left')
+
+    # Return the merged dataset.
     return df
