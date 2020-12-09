2d6b72235b655140b057544eaf1ee8635fb26bd8
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-12-09 16:19:00 -0600

    Updated acquire file

--- acquire.py
+++ acquire.py
@@ -7,17 +7,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
-import pyspark
-from pyspark.sql import SparkSession
-
-
 ###################### Acquire Riiid Data ########################
-
 
 def get_riiid_data():
     '''
     This function acquires `train`, `lectures`, and `questions` datasets and
     returns a merged spark dataframe.
+    
+    The merged dataset only contains HALF of users.
 
     Parameters
     ----------
@@ -25,21 +22,67 @@ def get_riiid_data():
     
     Returns
     -------
-    df :  pyspark.sql.dataframe.DataFrame
+    df_filtered : pandas.core.frame.DataFrame
     '''
+    if os.path.isfile('riiid_data.csv'):
+        return pd.read_csv('riiid_data.csv', index_col=False)
+    else:
+        # Dictionaries to cast column dtypes.
+        train_data_types_dict = {
+        'timestamp': np.int64,
+        'user_id': np.int32,
+        'content_id': np.int16,
+        'content_type_id': np.int16,
+        'task_container_id' : np.int16,
+        'user_answer' : np.int8,
+        'answered_correctly': np.int8,
+        'prior_question_elapsed_time': np.float16,
+        }
+        
+        lectures_data_types_dict = {
+        'lecture_id' : np.int16,
+        'tag' : np.int8,
+        'part' : np.int8
+        }
 
-    spark = SparkSession.builder.getOrCreate()
-    
-    # Acquire data
-    df_train = spark.read.csv('train.csv', header=True, inferSchema=True)
-    df_lectures = spark.read.csv('lectures.csv', header=True, inferSchema=True)
-    df_questions = spark.read.csv('questions.csv', header=True, inferSchema=True)
-    
-    # Left join df_train and df_lectures using `content_id` as the primary key.
-    df_merged = df_train.join(df_lectures, df_train.content_id == df_lectures.lecture_id, how='left')
-    
-    # Left join df_merged and df_questions using `content_id` as the primary key.
-    df = df_merged.join(df_questions, df_merged.content_id == df_questions.question_id, how='left')
+        questions_data_types_dict = {
+        'question_id' : np.int16,
+        'bundle_id' : np.int16,
+        'part' : np.int8
+        }
+        
+        # Acquire data
+        df_train = pd.read_csv('train.csv', dtype=train_data_types_dict)
+        df_lectures = pd.read_csv('lectures.csv', dtype=lectures_data_types_dict)
+        df_questions = pd.read_csv('questions.csv', dtype=questions_data_types_dict)
+        
+        # Left join df_train and df_lectures using `content_id` as the primary key.
+        df_merged = df_train.merge(df_lectures, left_on='content_id', right_on='lecture_id', how='left')
+        
+        # Left join df_merged and df_questions using `content_id` as the primary key.
+        df = df_merged.merge(df_questions, left_on='content_id', right_on='question_id', how='left')
+        
+        # Randomly select half of the users
+        all_users = df_train.user_id.value_counts().sort_index().index.tolist()
+        half_of_users_index = int(len(all_users) * .5)
+        df_filtered = df.loc[df.user_id.isin(df.user_id.sample(half_of_users_index, random_state=1))]
+        
+        # Fill in missing values with 0.
+        df_filtered = df_filtered.fillna(0)
+        
+        # Change the data types numeric columns.
+        df_filtered.lecture_id = df_filtered.lecture_id.astype(np.int16)
+        df_filtered.tag = df_filtered.tag.astype(np.int8)
+        df_filtered.part_x = df_filtered.part_x.astype(np.int8)
+        df_filtered.part_y = df_filtered.part_y.astype(np.int8)
+        df_filtered.question_id = df_filtered.question_id.astype(np.int16)
+        df_filtered.bundle_id = df_filtered.bundle_id.astype(np.int16)
+        df_filtered.lecture_id = df_filtered.lecture_id.astype(np.int32)
 
-    # Return the merged dataset.
-    return df
+        # Prefix part names with the originating dataframe name.
+        df_filtered.rename(columns={'part_x': 'lecture_part',
+                                    'part_y': 'question_part'},
+                           inplace=True)
+        
+        # Return the merged dataset.
+        return df_filtered
