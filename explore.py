3bb4ec5de6733ea7e4a9854394cc27e5a93335f2
Samuel Davila <samuelrdavila@gmail.com>
2020-12-11 16:03:23 -0600

    added RFE ranker

---
+++ explore.py
@@ -0,0 +1,47 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
+
+def rfe_ranker(train):
+    """
+    Accepts dataframe. Uses Recursive Feature Elimination to rank the given df's features in order of their usefulness in
+    predicting logerror with a logistic regression model.
+    """
+    non_target_vars = ['prior_question_had_explanation', 'user_acc_mean',
+       'mean_content_accuracy', 'mean_task_accuracy',
+       'mean_timestamp_accuracy_scaled', 'mean_priortime_accuracy_scaled',
+       'user_lectures_running_total_scaled', 'avg_user_q_time_scaled']
+    
+    target_var = ['answered_correctly']
+    
+    # creating logistic regression object
+    lr = LogisticRegression()
+
+    # fitting logistic regression model to features 
+    lr.fit(train[non_target_vars], train[target_var])
+
+    # creating recursive feature elimination object and specifying to only rank 1 feature as best
+    rfe = RFE(lr, 1)
+
+    # using rfe object to transform features 
+    x_rfe = rfe.fit_transform(train[non_target_vars], train[target_var])
+
+    # creating mask of selected feature
+    feature_mask = rfe.support_
+
+    # creating train df for rfe object 
+    rfe_train = train[non_target_vars]
+
+    # creating list of the top features per rfe
+    rfe_features = rfe_train.loc[:,feature_mask].columns.tolist()
+
+    # creating ranked list 
+    feature_ranks = rfe.ranking_
+
+    # creating list of feature names
+    feature_names = rfe_train.columns.tolist()
+
+    # create df that contains all features and their ranks
+    rfe_ranks_df = pd.DataFrame({'Feature': feature_names, 'Rank': feature_ranks})
+
+    # return df sorted by rank
+    return rfe_ranks_df.sort_values('Rank')
