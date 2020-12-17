57a13654c2594766fcf04fccee5d4fc69679d9ff
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-12-17 00:38:57 -0600

    update the prepare function to include all the new features

--- prepare.py
+++ prepare.py
@@ -26,7 +26,7 @@ def stats_content_id(df):
     content_stats.columns = ['mean_content_accuracy', 'question_content_asked', 'std_content_accuracy', 
                                 'median_content_accuracy', 'skew_content_accuracy']
 
-    return content_stats
+    return content_stats.round(2)
 
 def stats_task_container_id(df):
     '''
@@ -37,7 +37,40 @@ def stats_task_container_id(df):
     task_stats.columns = ['mean_task_accuracy', 'question_task_asked', 'std_task_accuracy', 
                             'median_task_accuracy', 'skew_task_accuracy']
 
-    return task_stats
+    return task_stats.round(2)
+
+def mean_tagcount_accuracy(df):
+    '''
+    To compute the mean accuracy according to the counts of the tags in a question
+    '''
+    tagcount_accuracy = df.groupby('tag_count').answered_correctly.mean().round(2).rename('mean_tagcount_accuracy')
+    return tagcount_accuracy
+
+def mean_tag_accuracy(df):
+    '''
+    To compute the mean accuracy according to the tag of a question
+    '''
+    tags_accuracy = df.groupby('tags').answered_correctly.mean().round(2).rename('mean_tags_accuracy')
+    return tags_accuracy
+
+def tag_features(train, validate, test):
+    '''
+    Accepts train, validate, test datasets and add to them the features mean tagcount accuracy and mean tag accuracy
+    '''
+    
+    tagcount_accuracy = mean_tagcount_accuracy(train)
+    tags_accuracy = mean_tag_accuracy(train)
+
+    train = train.merge(tagcount_accuracy, how='left', on='tag_count')
+    train = train.merge(tags_accuracy, how='left', on='tags')
+
+    validate = validate.merge(tagcount_accuracy, how='left', on='tag_count')
+    validate = validate.merge(tags_accuracy, how='left', on='tags')
+
+    test = test.merge(tagcount_accuracy, how='left', on='tag_count')
+    test = test.merge(tags_accuracy, how='left', on='tags')
+
+    return train, validate, test
 
 def merge_with_stats_train(df_train):
     '''
@@ -79,30 +112,6 @@ def handle_inf(df):
     m = df.prior_question_elapsed_time.apply(lambda i: 0 if i == np.inf else i)
     df.prior_question_elapsed_time = m
     return df
-    
-def merge_with_stats_train(df_train):
-    '''
-    To merger the train/validate/test with the new features generated from the train. 
-    '''   
-    content_stats = stats_content_id(df_train)
-    task_stats = stats_task_container_id(df_train)
-
-    train = df_train.merge(content_stats[['mean_content_accuracy']], how='left', on='content_id')
-    train = train.merge(task_stats[['mean_task_accuracy']], how='left', on='task_container_id')
-
-    return train
-
-def merge_with_stats_valortest(df_train, df_val_or_test):
-    '''
-    To merger the train/validate/test with the new features generated from the train. 
-    '''   
-    content_stats = stats_content_id(df_train)
-    task_stats = stats_task_container_id(df_train)
-
-    val_or_test = df_val_or_test.merge(content_stats[['mean_content_accuracy']], how='left', on='content_id')
-    val_or_test = val_or_test.merge(task_stats[['mean_task_accuracy']], how='left', on='task_container_id')
-
-    return val_or_test
 
 def sam_train_features(df):
     """
@@ -169,22 +178,13 @@ def sam_valtest_features(df, val_or_test):
     # returning df
     return val_or_test
 
-def drop_columns_train(df):
+def drop_columns(df):
     """
-    Accepts df. Drops various columns from train that are not needed after being used for other purposes.
+    Accepts df and drops various columns that are not needed for modeling.
     """
-    cols = ['user_id', 'content_id', 'content_type_id', 'timestamp',
-            'task_container_id', 'user_answer', 'prior_question_elapsed_time']
-    df.drop(columns=cols, inplace=True)
-    return df
-
-def drop_columns_valortest(df):
-    """
-    Accepts df. Drops various columns from validate df or test df that are not needed after being used for other purposes.
-    """
-    cols = ['user_id', 'timestamp', 'content_id', 'content_type_id', 
-            'task_container_id', 'user_answer', 'prior_question_elapsed_time']
-    df.drop(columns=cols, inplace=True)
+    cols = ['timestamp', 'user_id', 'content_id', 'task_container_id',
+            'question_id', 'bundle_id', 'part', 'tags', 'tag_count']
+    df = df.drop(columns=cols)
     return df
 
 def drop_lecture_rows(df):
@@ -230,19 +230,20 @@ def scale(train, validate, test, columns_to_scale):
     validate.drop(columns=columns_to_scale, inplace=True)
     test.drop(columns=columns_to_scale, inplace=True)
     
-    return scaler, train, validate, test
+    return train, validate, test
 
 def boolean_to_num(df):
     """
     Accepts df. Converts True and False values into 1's and 0's resepectively, within the 
     question_had_explanation column.
     """
+    df = df.fillna(False)
     m = df.question_had_explanation.apply(lambda i: 1 if i == True else 0)
     df.question_had_explanation = m
     return df
 
 
-def tag_bundle_features(train, validate, test):
+def part_bundle_features(train, validate, test):
     '''
     This function accepts the train, validate, and test sets.
     Returns new features on train, validate, and test using population stats from the training set.
@@ -266,33 +267,34 @@ def tag_bundle_features(train, validate, test):
     merged_test = test.merge(bundle_accuracy, left_on='bundle_id', right_on='bundle_id', how='left')
     
     # Calculate the average part accuracy
-    tag_accuracy = train.groupby(['question_part'])['answered_correctly'].agg(['mean']).round(2).reset_index()
-    tag_accuracy.columns = ['question_part', 'mean_part_accuracy']
+    tag_accuracy = train.groupby(['part'])['answered_correctly'].agg(['mean']).round(2).reset_index()
+    tag_accuracy.columns = ['part', 'mean_part_accuracy']
     
     # Add average part accuracy
-    train_df = merged_train.merge(tag_accuracy, left_on='question_part', right_on='question_part')
-    validate_df = merged_validate.merge(tag_accuracy, left_on='question_part', right_on='question_part')
-    test_df = merged_test.merge(tag_accuracy, left_on='question_part', right_on='question_part')
+    train_df = merged_train.merge(tag_accuracy, left_on='part', right_on='part')
+    validate_df = merged_validate.merge(tag_accuracy, left_on='part', right_on='part')
+    test_df = merged_test.merge(tag_accuracy, left_on='part', right_on='part')
     
     # Calculate the mean container accuracy for each part
-    tag_bundles = train.groupby(['question_id', 'task_container_id', 'question_part'])['answered_correctly'].mean().round(2).reset_index()
+    tag_bundles = train.groupby(['question_id', 'task_container_id', 'part'])['answered_correctly'].mean().round(2).reset_index()
     tag_bundles.rename(columns={'answered_correctly': 'mean_container_part_accuracy'}, inplace=True)
+    tag_bundles.drop(columns='question_id', inplace=True)
     
-    # Add mean container part accuracy
-    train_set = train_df.merge(tag_bundles, how='left', 
-                               left_on=['task_container_id', 'question_part'], 
-                               right_on=['task_container_id', 'question_part'])
+#     # Add mean container part accuracy
+#     train_set = train_df.merge(tag_bundles, how='left', 
+#                                left_on=['task_container_id', 'part'], 
+#                                right_on=['task_container_id', 'part'])
     
-    validate_set = validate_df.merge(tag_bundles, how='left', 
-                                     left_on=['task_container_id', 'question_part'], 
-                                     right_on=['task_container_id', 'question_part'])
+#     validate_set = validate_df.merge(tag_bundles, how='left', 
+#                                      left_on=['task_container_id', 'part'], 
+#                                      right_on=['task_container_id', 'part'])
     
-    test_set = test_df.merge(tag_bundles, how='left', 
-                             left_on=['task_container_id', 'question_part'], 
-                             right_on=['task_container_id', 'question_part'])
+#     test_set = test_df.merge(tag_bundles, how='left', 
+#                              left_on=['task_container_id', 'part'], 
+#                              right_on=['task_container_id', 'part'])
 
     
-    return train_set, validate_set, test_set
+    return train_df, validate_df, test_df
 
 ####### COMPLETE PREP FUNCTION ########
 
@@ -300,8 +302,6 @@ def prep_riiid(df_train, df_validate, df_test):
     """
     Accepts train, validate and test DFs. Returns all three fully prepped for exploration.
     """
-    # Add new features to train, validate, and test
-    df_train, df_validate, df_test = tag_bundle_features(df_train, df_validate, df_test)
     
     # Drop the columns merged from questions.csv and lectures.csv
     cols = ['lecture_id', 'tag', 'lecture_part', 'type_of', 'question_id',
@@ -330,21 +330,35 @@ def prep_riiid(df_train, df_validate, df_test):
     train = drop_lecture_rows(train)
     validate = drop_lecture_rows(validate)
     test = drop_lecture_rows(test)
+
+    # Reading questions csv
+    df_ques = pd.read_csv('questions_with_tag_counts.csv', index_col=0)
+
+    # Merge train/validate/test with df_ques
+    train = train.merge(df_ques, how='left', left_on='content_id', right_on='question_id')
+    validate = validate.merge(df_ques, how='left', left_on='content_id', right_on='question_id')
+    test = test.merge(df_ques, how='left', left_on='content_id', right_on='question_id')
+
+    # Drop the redundant column to save memory
+    train.drop(columns=['content_type_id', 'user_answer', 'prior_question_elapsed_time', 'correct_answer'], inplace=True)
+    validate.drop(columns=['content_type_id', 'user_answer', 'prior_question_elapsed_time', 'correct_answer'], inplace=True)
+    test.drop(columns=['content_type_id', 'user_answer', 'prior_question_elapsed_time', 'correct_answer'], inplace=True)
     
-    # Merge the new features genereated from Shi
+    # Add features: mean part accuracy and mean bundle accuracy
+    train, validate, test = part_bundle_features(train, validate, test)
+    
+    # Add features: mean content accuracy and mean task accuracy
     train = merge_with_stats_train(train)
     validate = merge_with_stats_valortest(train, validate)
     test = merge_with_stats_valortest(train, test)
     
-    # drop columns no longer needed
-    train = drop_columns_train(train)
-    validate = drop_columns_valortest(validate)
-    test = drop_columns_valortest(test)
+    # Add features: mean tagcount accuracy and mean tag accuracy    
+    train, validate, test = tag_features(train, validate, test)
     
     # fill nulls created from merging
     validate = fill_nulls(validate)
     test = fill_nulls(test)
-
+    
     # shift prior question had explanation to current question
     train.prior_question_had_explanation = train.prior_question_had_explanation.shift(-1)
     validate.prior_question_had_explanation = validate.prior_question_had_explanation.shift(-1)
@@ -354,18 +368,22 @@ def prep_riiid(df_train, df_validate, df_test):
     validate = validate.rename(columns={"prior_question_had_explanation": "question_had_explanation"})
     test = test.rename(columns={"prior_question_had_explanation": "question_had_explanation"})
 
+    # drop the column q_time in train_s for modeling
+    train_s = train.drop(columns='q_time')
+    
+    # drop columns no longer needed for the purpose of modeling
+    train_s = drop_columns(train_s)
+    validate_s = drop_columns(validate)
+    test_s = drop_columns(test)
+    
     # convert boolean to num
-    train = boolean_to_num(train)
-    validate = boolean_to_num(validate)
-    test = boolean_to_num(test)
+    train_s = boolean_to_num(train_s)
+    validate_s = boolean_to_num(validate_s)
+    test_s = boolean_to_num(test_s)
     
     # scale columns
     columns_to_scale = ['user_lectures_running_total', 'avg_user_q_time']
-
-    scaler, train_s, validate_s, test_s = scale(train, validate, test, columns_to_scale)
-
-    # drop the column q_time in train_s for modeling
-    train_s.drop(columns='q_time', inplace=True)
+    train_s, validate_s, test_s = scale(train_s, validate_s, test_s, columns_to_scale)
     
     # returning DFs
     return train, validate, test, train_s, validate_s, test_s
